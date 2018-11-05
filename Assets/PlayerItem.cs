d87c96611133d17f2af7a3a194516b6ca27a9919
FFang <564490014@qq.com>
2018-11-05 10:27:51 +0800

    暂时做到这就行了 后面根据项目再加
    不知道后面要干啥

---
+++ Assets/PlayerItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerItem : MonoBehaviour
+{
+
+    public Text ScoreText;
+
+    private int score = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ScoreText.text = score.ToString();
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "FallBall")
+        {
+            score++;
+        }
+
+        if (collision.gameObject.tag == "Bomb")
+        {
+            score=0;
+        }
+            
+    }
+
+    
+}
